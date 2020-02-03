858af5f77759305ae7e81e0b44c7c20631f96262
eric199wen <eric199.wen@gmail.com>
2020-02-02 17:01:45 -0800

    Support finding controller and routing

--- lib/rulers.rb
+++ lib/rulers.rb
@@ -1,10 +1,33 @@
 require "rulers/array"
+require "rulers/routing"
 
 module Rulers
   class Application
     def call(env)
-      [200, {'Content-Type' => 'text/html'},
-        ["Hello from Ruby on Rulers!"]]
+      if env['PATH_INFO'] == '/favicon.ico'
+        return [404, {'Content-Type' => 'text/html'}, []]
+      end
+
+      klass, act = get_controller_and_action(env)
+      controller = klass.new(env)
+      begin
+        text = controller.send(act)
+      rescue => exception
+        puts exception
+        return [500, {'Content-Type' => 'text/html'}, ['Something went wrong!']]
+      end
+      
+      [200, {'Content-Type' => 'text/html'},[text]]
+    end
+  end
+
+  class Controller
+    def initialize(env)
+      @env = env
+    end
+
+    def env
+      @env
     end
   end
 end
